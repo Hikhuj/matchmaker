97af6fe489dc8e6efb7a9aa1897ac099fff23333
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2018-07-03 21:28:42 -0600

    Refactor: 1 continues

--- main.py
+++ main.py
@@ -26,10 +26,6 @@ init()
 
 def message_id_matcher_welcome():
 
-	'''
-		Message to user
-	'''
-
 	print("")
 	print(Fore.YELLOW + "* --------------------------------------------------- *")
 	print(Fore.YELLOW + "|                                                     |")
@@ -41,10 +37,6 @@ def message_id_matcher_welcome():
 
 
 def message_id_matcher_bye():
-
-	'''
-		Message to user
-	'''
 
 	print("")
 	print(Fore.GREEN + "* --------------------------------------------------- *")
@@ -58,17 +50,27 @@ def message_id_matcher_bye():
 
 def verify_if_file_in_path_exists(file_path):
 
-	'''
-		Function read where the file is on the root project only.
-	'''
-
+	# Function read where the file is on the root project only.
 	result = os.path.isfile(file_path)
+	print(result)
+	return result
 
+
+def request_file_A_path():
+
+	print("Please provide the file that works as base document...")
+	result = get_relative_path()
+	return result
+
+
+def request_file_B_path():
+
+	print("Please provide the file that will be compare...")
+	result = get_relative_path()
 	return result
 
 
 # Request compression of relative paths
-
 def get_relative_path():
 
 	# Variables
@@ -106,85 +108,10 @@ def get_relative_path():
 		print(Style.RESET_ALL)
 		get_relative_path()
 
-	return absFilePath
-
-
-'''
-def get_current_relative_path_base_file():
-
-	# Variables declaration
-	result = ""
-	absFilePath = ""
-
 	print("")
 
-	# Request the file name to work
-	file_name = input("1) Add the file name you will use as base (use the format ==> fileName.extension): ")
-	is_a_csv = validator_check_file_extension(file_name)
+	return result
 
-	# Verification
-	if is_a_csv == True:
-		absFilePath = os.path.abspath(file_name)
-		csv_exists = verify_if_file_in_path_exists(absFilePath)
-
-		# If the file exist
-		if csv_exists == True:
-			result = absFilePath
-		else:
-			print(Fore.RED + "* --------------------------------------------------- *")
-			print(Fore.RED + "|       The file does not exist, please add one       |")
-			print(Fore.RED + "* --------------------------------------------------- *")
-			print(Style.RESET_ALL)
-			get_current_relative_path_base_file()
-
-	# If the file is not a .csv extension
-	else:
-		print(Fore.RED + "* --------------------------------------------------- *")
-		print(Fore.RED + "|    The file you are trying to add is not a '.csv'   |")
-		print(Fore.RED + "|           Please try adding a CSV file only         |")
-		print(Fore.RED + "* --------------------------------------------------- *")
-		print(Style.RESET_ALL)
-		get_current_relative_path_base_file()
-
-	return absFilePath
-
-
-def get_current_relative_path_joined_file():
-
-	# Declaration
-	absFilePath = ""
-
-	print("")
-	# Request the file name to work
-	file_name = input("2) Add the file name you will try to match (use the format ==> fileName.extension): ")
-	is_a_csv = validator_check_file_extension(file_name)
-
-	# Verification
-	if is_a_csv == True:
-		absFilePath = os.path.abspath(file_name)
-		csv_exists = verify_if_file_in_path_exists(absFilePath)
-
-		# If the file exist
-		if csv_exists == True:
-			result = absFilePath
-		else:
-			print(Fore.RED + "* --------------------------------------------------- *")
-			print(Fore.RED + "|       The file does not exist, please add one       |")
-			print(Fore.RED + "* --------------------------------------------------- *")
-			print(Style.RESET_ALL)
-			get_current_relative_path_joined_file()
-
-	# If the file is not a .csv extension
-	else:
-		print(Fore.RED + "* --------------------------------------------------- *")
-		print(Fore.RED + "|    The file you are trying to add is not a '.csv'   |")
-		print(Fore.RED + "|           Please try adding a CSV file only         |")
-		print(Fore.RED + "* --------------------------------------------------- *")
-		print(Style.RESET_ALL)
-		get_current_relative_path_joined_file()
-
-	return absFilePath
-'''
 
 def validator_check_file_extension(csv_file_name):
 
@@ -205,11 +132,16 @@ def csv_file_reader(file_to_read_A, file_to_read_B):
 
 		position = 1
 		for row in file_A:
-			print("=======> Processing line: " + str(position))
+			# Restart always with clean array
 			full_row = []
+			print("=======> Processing line: " + str(position))
 			full_row = search_id_in_file(row[0], file_to_read_B, position)
-			row.extend(full_row)
+			# row.extend(full_row)
+			# row.append(full_row)
+			# row.insert(7, full_row)
+
 			write_on_file(row)
+			# Increase row position
 			position += 1
 
 
@@ -225,10 +157,8 @@ def search_id_in_file(line_id, file_to_read_B, position):
 		for row in file_B:
 			if row[0] == line_id:
 				result = row
-				print(">>> Id found in position: " + str(position) + ", matching...")
+				print(">>> Id found: " + str(position) + ", matching...")
 				print(".")
-				print(".")
-				# I will make that return and Array, so in an upper level I can merge it.
 				break
 
 	return result
@@ -241,27 +171,7 @@ def write_on_file(row):
 		clean_file.writerow(row)
 
 
-def request_file_A_path():
-
-	print("Please provide the file that works as base document...")
-	result = get_relative_path()
-	return result
-
-
-def request_file_B_path():
-
-	print("Please provide the file that will be compare...")
-	result = get_relative_path()
-	return result
-
-
 def interactive_menu():
-
-	# Colored all text on red
-	# print(Fore.RED + 'some red text')
-
-	# Welcome message
-	message_id_matcher_welcome()
 
 	# Get base file to compare
 	file_A_path = request_file_A_path()
@@ -272,13 +182,16 @@ def interactive_menu():
 	# Process the two CSV to be compare
 	csv_file_reader(file_A_path, file_B_path);
 
-	# Good bye message
-	message_id_matcher_bye()
-
 ### Main
 def main():
 
+	# Welcome message
+	message_id_matcher_welcome()
+
 	interactive_menu()
+
+	# Good bye message
+	message_id_matcher_bye()
 
 	# Close Colorama use
 	deinit()
